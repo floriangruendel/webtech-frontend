711250eee780dcc6f709b163cbf89f757ff816e6
flogruendel <florian.gruendel98@gmail.com>
2022-01-06 20:54:10 +0100

    updating Todos is working but buggy

--- src/views/Todo.vue
+++ src/views/Todo.vue
@@ -11,7 +11,38 @@
           </div>
           <p style="font-size:18px" class="mb-1" align="left">{{ levy.discription }}</p>
           <p style="font-size:13px" class="mb-1" align="left">{{ levy.modul }}</p>
-          <button type="button" class="btn btn-outline-secondary">Edit</button>
+          <button type="button" class="btn btn-outline-secondary" data-bs-toggle="offcanvas" data-bs-target="#offcanvasRight2" aria-controls="offcanvasRight2">Edit</button>
+          <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasRight2" aria-labelledby="offcanvasRight2Label">
+            <div class="offcanvas-header">
+              <h5 id="offcanvasRight2Label">Edit TODO</h5>
+              <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+            </div>
+            <div class="offcanvas-body">
+              <form class="test-start needs-validation" novalidate>
+                <input class="form-control" type="text" placeholder="Title" aria-label="default input example" v-model="title" required>
+                <div class="invalid-feedback" align="left">
+                  Please provide a title.
+                </div>
+                <br>
+                <input class="form-control" type="text" placeholder="Discription" aria-label="default input example" v-model="discription" required>
+                <div class="invalid-feedback" align="left">
+                  Please provide a discription.
+                </div>
+                <br>
+                <input class="form-control" type="text" placeholder="Modul" aria-label="default input example" v-model="modul" required>
+                <div class="invalid-feedback" align="left">
+                  Please provide a modul.
+                </div>
+                <br>
+                <input class="form-control" type="text" placeholder="Deadline (yyyy-mm-dd)" aria-label="default input example" v-model="deadline" required>
+                <div class="invalid-feedback" align="left">
+                  Please provide a deadline.
+                </div>
+                <br>
+                <button class="btn btn-outline-success me-3" type="submit" @click="updateLevy(levy.id)">Confirm</button>
+              </form>
+            </div>
+          </div>
           <i>&nbsp;</i>
           <button type="button" class="btn btn-outline-danger" @click="deleteLevy(levy.id)">Delete</button>
         </a>
@@ -84,8 +115,8 @@ export default {
           deadline: this.deadline,
           importance: 'normal'
         })
-        //   const endpoint = 'http://localhost:8080/api/v1/levies'
-        const endpoint = process.env.VUE_APP_BACKEND_BASE_URL + '/api/v1/levies'
+        const endpoint = 'http://localhost:8080/api/v1/levies'
+        //  const endpoint = process.env.VUE_APP_BACKEND_BASE_URL + '/api/v1/levies'
         const requestOptions = {
           method: 'POST',
           headers: headers,
@@ -97,10 +128,36 @@ export default {
       }
     },
     deleteLevy (id) {
-      //  const endpoint = 'http://localhost:8080/api/v1/levies/' + id
-      const endpoint = process.env.VUE_APP_BACKEND_BASE_URL + '/api/v1/levies/' + id
+      const endpoint = 'http://localhost:8080/api/v1/levies/' + id
+      //  const endpoint = process.env.VUE_APP_BACKEND_BASE_URL + '/api/v1/levies/' + id
       const requestOptions = {
         method: 'DELETE',
+        redirect: 'follow'
+      }
+
+      fetch(endpoint, requestOptions)
+        .then(response => response.text())
+        .then(result => console.log(result))
+        .catch(error => console.log('error', error))
+    },
+    updateLevy (id) {
+      const myHeaders = new Headers()
+      myHeaders.append('Content-Type', 'application/json')
+
+      const raw = JSON.stringify({
+        title: this.title,
+        discription: this.discription,
+        modul: this.modul,
+        deadline: this.deadline,
+        importance: 'normal'
+      })
+
+      const endpoint = 'http://localhost:8080/api/v1/levies/' + id
+      //  const endpoint = process.env.VUE_APP_BACKEND_BASE_URL + '/api/v1/levies/' + id
+      const requestOptions = {
+        method: 'PUT',
+        headers: myHeaders,
+        body: raw,
         redirect: 'follow'
       }
 
@@ -112,7 +169,7 @@ export default {
     validate () {
       let valid = true
       // Fetch all the forms we want to apply custom Bootstrap validation styles to
-      var forms = document.querySelectorAll('.needs-validation')
+      const forms = document.querySelectorAll('.needs-validation')
       // Loop over them and prevent submission
       Array.prototype.slice.call(forms)
         .forEach(function (form) {
@@ -129,8 +186,8 @@ export default {
     }
   },
   mounted () {
-    //  const endpoint = 'http://localhost:8080/api/v1/levies'
-    const endpoint = process.env.VUE_APP_BACKEND_BASE_URL + '/api/v1/levies'
+    const endpoint = 'http://localhost:8080/api/v1/levies'
+    //  const endpoint = process.env.VUE_APP_BACKEND_BASE_URL + '/api/v1/levies'
     const requestOptions = {
       method: 'GET',
       redirect: 'follow'
